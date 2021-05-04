63ffa8a1d6f8394ef4490fa15bb097a86b64cb70
David Andreasson <647640+skogdoom@users.noreply.github.com>
2021-05-04 20:30:00 +0200

    parse out all podcasts

--- export-opml.py
+++ export-opml.py
@@ -1,9 +1,20 @@
 import xml.dom.minidom
+import xml.etree.ElementTree as ET
 
 
 def export(filename):
-    doc = xml.dom.minidom.parse(filename)
-    print(doc.nodeName)
+    tree = ET.parse(filename)
+    root = tree.getroot()
+    elements = root.find('array/dict/array/dict/array')
+
+    podcasts = []
+    for element in elements:
+        keyValues = [v.text for v in element]
+        iterator = iter(keyValues)
+        podcast = [(k, v) for k, v in zip(iterator, iterator)]
+        podcasts.append(podcast)
+
+    print(len(podcasts))
 
 
 if __name__ == '__main__':
