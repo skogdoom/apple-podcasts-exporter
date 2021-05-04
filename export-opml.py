1dee708254ee9d22db8423978b5b2ea30029fdf9
David Andreasson <647640+skogdoom@users.noreply.github.com>
2021-05-04 21:13:55 +0200

    minor cleanup

--- export-opml.py
+++ export-opml.py
@@ -2,11 +2,6 @@ from xml.dom.minidom import Document
 from xml.etree import ElementTree as ET
 
 
-def save_file(out_filename, xml_str):
-    with open(out_filename, "w") as f:
-        f.write(xml_str)
-
-
 def export(in_filename, out_filename):
     podcasts = parse_plist(in_filename)
     opml = to_opml(podcasts)
@@ -17,12 +12,13 @@ def parse_plist(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     elements = root.find('array/dict/array/dict/array')
+
     podcasts = []
     for element in elements:
-        keyValues = [v.text for v in element]
-        iterator = iter(keyValues)
+        iterator = iter([v.text for v in element])
         podcast = dict((k, v) for k, v in zip(iterator, iterator))
         podcasts.append(podcast)
+
     return podcasts
 
 
@@ -43,6 +39,11 @@ def to_opml(podcasts):
         body.appendChild(outline)
 
     return xml
+
+
+def save_file(out_filename, xml_str):
+    with open(out_filename, "w") as f:
+        f.write(xml_str)
 
 
 if __name__ == '__main__':
