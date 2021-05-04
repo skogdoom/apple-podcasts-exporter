7f926bc2cdb010ab74f4050671e06ad09adb4781
David Andreasson <647640+skogdoom@users.noreply.github.com>
2021-05-04 21:10:50 +0200

    save file

--- export-opml.py
+++ export-opml.py
@@ -2,26 +2,28 @@ from xml.dom.minidom import Document
 from xml.etree import ElementTree as ET
 
 
-def export(filename):
+def save_file(out_filename, xml_str):
+    with open(out_filename, "w") as f:
+        f.write(xml_str)
+
+
+def export(in_filename, out_filename):
+    podcasts = parse_plist(in_filename)
+    opml = to_opml(podcasts)
+    save_file(out_filename, opml.toprettyxml(indent="\t"))
+
+
+def parse_plist(filename):
     tree = ET.parse(filename)
     root = tree.getroot()
     elements = root.find('array/dict/array/dict/array')
-
     podcasts = []
     for element in elements:
         keyValues = [v.text for v in element]
         iterator = iter(keyValues)
         podcast = dict((k, v) for k, v in zip(iterator, iterator))
         podcasts.append(podcast)
-
-    opml = to_opml(podcasts)
-    print(opml.toprettyxml(indent="\t"))
-
-    # TODO save file
-    # save_path_file = "gfg.xml"
-
-    # with open(save_path_file, "w") as f:
-    #    f.write(xml_str)
+    return podcasts
 
 
 def to_opml(podcasts):
@@ -45,4 +47,7 @@ def to_opml(podcasts):
 
 if __name__ == '__main__':
     # File containing podcasts is located here: ~/Library/Containers/com.apple.podcasts/Data/Documents/PodcastsDB.plist
-    export('/Users/davidandreasson/Library/Containers/com.apple.podcasts/Data/Documents/PodcastsDB.plist')
+    export(
+        '/Users/davidandreasson/Library/Containers/com.apple.podcasts/Data/Documents/PodcastsDB.plist',
+        '/Users/davidandreasson/Downloads/podcasts.opml'
+    )
